ffa061b8e6f38f32d10f99b961f4e55877763244
mkjsym <mkjsym@naver.com>
2024-08-14 15:44:14 +0900

    2024.08.14

--- 2024.08.14.py
+++ 2024.08.14.py
@@ -121,3 +121,100 @@
 
 
 #단어의 개수
+# S = input()
+
+# cnt = 0
+# beforeNull = False
+# if (S[0] != " "):
+#     cnt += 1
+# else:
+#     beforeNull == True
+
+# for i in range(len(S)):
+#     if (S[i] != " "):
+#         if (beforeNull == True):
+#             cnt += 1
+#         beforeNull = False
+#     else:
+#         beforeNull = True
+
+# print(cnt)
+
+
+#상수
+# a, b = input().split()
+
+# a = a[::-1]
+# b = b[::-1]
+
+# if (int(a) > int(b)):
+#     print(a)
+# else:
+#     print(b)
+
+
+#다이얼
+# S = input()
+
+# sum = 0
+# for i in range(len(S)):
+#     if ((64 < ord(S[i])) and (ord(S[i]) <= 67)):
+#         sum += 3
+#     elif ((67 < ord(S[i])) and (ord(S[i]) <= 70)):
+#         sum += 4
+#     elif ((70 < ord(S[i])) and (ord(S[i]) <= 73)):
+#         sum += 5
+#     elif ((73 < ord(S[i])) and (ord(S[i]) <= 76)):
+#         sum += 6
+#     elif ((76 < ord(S[i])) and (ord(S[i]) <= 79)):
+#         sum += 7
+#     elif ((79 < ord(S[i])) and (ord(S[i]) <= 83)):
+#         sum += 8
+#     elif ((83 < ord(S[i])) and (ord(S[i]) <= 86)):
+#         sum += 9
+#     elif ((86 < ord(S[i])) and (ord(S[i]) <= 90)):
+#         sum += 10
+
+# print(sum)
+
+
+#그대로 출력하기
+# while True:
+#     try:
+#         S = input()
+#         print(S)
+#     except:
+#         break
+
+
+#별 찍기 - 7
+# N = int(input())
+
+# pivot = ((N * 2) - 1) // 2
+# for i in range(N*2 - 1):
+#     if (i <= pivot):
+#         for j in range(N-1-i):
+#             print(" ", end="")
+#         for j in range(1+2*i):
+#             print("*", end="")
+#     else:
+#         for j in range(i-pivot):
+#             print(" ", end="")
+#         for j in range(N*2-1-2*(i-pivot)):
+#             print("*", end="")
+#     print()
+
+
+#팰린드롬인지 확인하기
+S = input()
+
+re = len(S) // 2
+print(re)
+
+result = 1
+for i in range(re):
+    if (S[i] != S[-(i+1)]):
+        result = 0
+        break
+
+print(result)
