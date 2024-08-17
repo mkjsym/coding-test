d55c6eaf0cc149d2c2a90fa5c1c06ecbfb1b4009
YM\mkjsy <mkjsym@naver.com>
2024-08-17 19:36:21 +0900

    2024.08.17

--- YM/2024.08.17.py
+++ YM/2024.08.17.py
@@ -1,1 +1,42 @@
-#
+#세로읽기
+# matrix = []
+
+# for i in range(5):
+#     matrix.append(input())
+
+# result = []
+# for i in range(15):
+#     for j in range(5):
+#         if (i < len(matrix[j])):
+#             result.append(matrix[j][i])
+
+# for i in result:
+#     print(i, end="")
+
+
+#색종이
+# board = []
+# for i in range(100):
+#     row = []
+#     for j in range(100):
+#         row.append(0)
+#     board.append(row)
+
+# N = int(input())
+
+# for i in range(N):
+#     x, y = map(int, input().split())
+
+#     for j in range(10):
+#         for k in range(10):
+#             board[j+x][k+y] = 1
+
+# sum = 0
+# for i in range(100):
+#     for j in range(100):
+#         sum += board[i][j]
+
+# print(sum)
+
+
+#진법 변환
