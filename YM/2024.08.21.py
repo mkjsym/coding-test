2b32b7352acd1988bb5ad5e865827a5d2ffee6c7
mkjsym <mkjsym@naver.com>
2024-08-21 15:50:57 +0900

    2024.08.21

--- YM/2024.08.21.py
+++ YM/2024.08.21.py
@@ -1,1 +1,21 @@
 #백준 2292 벌집
+# N = int(input())
+
+# range = 6
+# start = 1
+# end = 1
+# i = 1
+# while (1):
+#     if (N >= start and N <= end):
+#         break
+
+#     start = end + 1
+#     end = start + range * i - 1
+#     i += 1
+
+# print(i)
+
+
+#백준 1193 분수찾기
+X = int(input())
+
