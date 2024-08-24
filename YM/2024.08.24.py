9d3e2ada40977a86b74133c561fd7d5ff2f5d317
YM\mkjsy <mkjsym@naver.com>
2024-08-24 21:27:51 +0900

    2024.08.24

---
+++ YM/2024.08.24.py
@@ -0,0 +1,60 @@
+#백준 24262 알고리즘 수업 - 알고리즘의 수행 시간 1
+# n = int(input())
+
+# print(1)
+# print(0)
+
+
+#백준 24263 알고리즘 수업 - 알고리즘의 수행 시간 2
+# n = int(input())
+
+# time = n
+# degree = 1
+
+# print(time)
+# print(degree)
+
+
+#백준 24264 알고리즘 수업 - 알고리즘의 수행 시간 3
+# n = int(input())
+
+# time = n * n
+# degree = 2
+
+# print(time)
+# print(degree)
+
+
+#백준 24265 알고리즘 수업 - 알고리즘의 수행 시간 4
+# n = int(input())
+
+# inner = 0
+# for i in range(n-1):
+#     inner += n - (i + 1)
+# time = inner
+# degree = 2
+
+# print(time)
+# print(degree)
+
+
+#백준 24266 알고리즘 수업 - 알고리즘의 수행 시간 5
+# n = int(input())
+
+# time = n * n * n
+# degree = 3
+
+# print(time)
+# print(degree)
+
+
+#백준 24267 알고리즘 수업 - 알고리즘의 수행 시간 6
+# n = int(input())
+
+# result = n * (n - 1) * (n - 2) // 6
+
+# print(result)
+# print(3)
+
+
+#
