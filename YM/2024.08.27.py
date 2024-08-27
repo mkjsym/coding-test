648d96abd8dc1a801728d5878f6279a10bdef13a
YM\mkjsy <mkjsym@naver.com>
2024-08-27 23:03:35 +0900

    2024.08.27

---
+++ YM/2024.08.27.py
@@ -0,0 +1,24 @@
+#백준 1193 분수찾기
+# X = int(input())
+
+# end = 0
+# line = 0
+# while (X > end):
+#     line += 1
+#     end += line
+
+# diff = end - X
+
+# x = 0
+# y = 0
+# if (line % 2 != 0):
+#     y = line - diff
+#     x = line + 1 - y
+# else:
+#     x = line - diff
+#     y = line + 1 - x
+
+# print(str(x) + '/' + str(y))
+
+
+#
