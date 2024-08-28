e6a0b4ff40ee62f5fef27352559c2c197345cf48
YM\mkjsy <mkjsym@naver.com>
2024-08-28 21:37:19 +0900

    2024.08.28

---
+++ YM/2024.08.28.py
@@ -0,0 +1,19 @@
+#백준 1181 단어 정렬
+# N = int(input())
+
+# chars = set()
+# for i in range(N):
+#     name, stat = map(str, input().split())
+#     if (stat == 'enter'):
+#         chars.add(name)
+#     else:
+#         chars.remove(name)
+
+# chars = list(chars)
+# chars.sort(reverse = True)
+
+# for i in chars:
+#     print(i)
+
+
+#
