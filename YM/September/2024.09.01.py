af2837c3c6a73e5c06514b2d2811b9c5d07c8811
전 영민 <ym@jeon-ui-MacBookAir.local>
2024-09-01 17:20:21 +0900

    2024.09.01

--- YM/September/2024.09.01.py
+++ YM/September/2024.09.01.py
@@ -1,1 +1,32 @@
-#
+#백준 2839 설탕배달
+# def returnXY(n):
+#     x = 0
+#     while (x * 3 <= n):
+#         y = 0
+#         while(y * 5 <= n):
+#             if (x * 3 + y * 5 == n):
+#                 return x + y
+
+#             y += 1
+#         x += 1
+
+#     return -1
+
+# N = int(input())
+
+# print(returnXY(N))
+
+
+#백준 1427 소트인사이드
+# N = list(input())
+
+# N.sort(reverse = True)
+
+# result = ""
+# for i in N:
+#     result = result + i
+
+# print(result)
+
+
+#백준 11650 좌표 정렬하기
