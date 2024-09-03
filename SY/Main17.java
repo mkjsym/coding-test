7b41785209bb7cefebd57a9513256c803e96d6be
so-yeon1 <psy92411@gmail.com>
2024-09-03 16:15:28 +0900

    2024.09.03

---
+++ SY/Main17.java
@@ -0,0 +1,41 @@
+// 2024.09.03
+package SY;
+
+/**************** 18870. 백준 ****************/
+/*
+ * TreeMap(정렬된 map) 이용 : 중복제거+정렬
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main17 {
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		TreeMap <Integer, Integer> map = new TreeMap<>();
+		int [] arr = new int[N];
+		
+		// 1. 값 넣기
+		for(int i=0; i<N; i++) {	
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, 0);
+			arr[i] = num;
+		}
+		
+		// 2. map의 value에 정렬 결과 넣기
+		int j=0;
+		for(int key: map.keySet()) {
+			map.put(key, j++);
+		}
+		
+		// 3. 결과값 출력
+		for(int i=0; i<N; i++) {
+			sb.append(map.get(arr[i])).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
