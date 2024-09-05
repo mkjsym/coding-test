2741b35ba0c025e860be974a46dc23444ea2fe74
mkjsym <mkjsym@naver.com>
2024-09-05 16:12:20 +0900

    2024.09.05

--- YM/September/2024.09.05.py
+++ YM/September/2024.09.05.py
@@ -1,23 +1,26 @@
 #백준 10814 나이순 정렬
-import sys
+# import sys
 
-N = int(sys.stdin.readline().strip())
+# N = int(sys.stdin.readline().strip())
 
-ages = set()
-dic = {}
-for _ in range(N):
-    age, name = map(str, sys.stdin.readline().strip().split())
+# ages = set()
+# dic = {}
+# for _ in range(N):
+#     age, name = map(str, sys.stdin.readline().strip().split())
 
-    if (int(age) in ages):
-        dic[int(age)] = dic[int(age)] + ' ' + name
-    else:
-        ages.add(int(age))
-        dic[int(age)] = name
+#     if (int(age) in ages):
+#         dic[int(age)] = dic[int(age)] + ' ' + name
+#     else:
+#         ages.add(int(age))
+#         dic[int(age)] = name
 
-dic = dict(sorted(dic.items()))
+# dic = dict(sorted(dic.items()))
 
-for i, v in dic.items():
-    temp = v.split()
+# for i, v in dic.items():
+#     temp = v.split()
 
-    for j in temp:
-        print(i, j)
+#     for j in temp:
+#         print(i, j)
+
+
+#
