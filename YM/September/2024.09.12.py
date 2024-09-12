eaf951cd14af94751969f961747f6f4a5884ca4f
mkjsym <mkjsym@naver.com>
2024-09-12 17:33:49 +0900

    2024.09.12

---
+++ YM/September/2024.09.12.py
@@ -0,0 +1,29 @@
+#백준 18258 큐 2
+import sys
+
+class queue:
+    def __init__():
+        data = []
+    
+    def push():
+        asd
+    
+    def pop():
+        asd
+
+    def size():
+        asd
+    
+    def empty():
+        asd
+
+    def front():
+        asd
+    
+    def back():
+        asd
+
+N = int(sys.stdin.readline().strip())
+
+for _ in range(N):
+    
