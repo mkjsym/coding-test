14477e90e1a17ac4adfab00491d9159201de95e3
mkjsym <mkjsym@naver.com>
2024-09-16 23:23:21 +0900

    2024.09.16

--- YM/September/2024.09.16.py
+++ YM/September/2024.09.16.py
@@ -1,21 +1,38 @@
-#백준 1934 최소공배수
+#백준 1934 최소공배수#################################################
+# import sys
+
+#유클리드 호제법에 대하여... 자세히 공부해야겠다.
+# def returnLCM(a, b):
+#     while (b != 0):
+#         r = a % b
+#         a = b
+#         b = r
+    
+#     return a
+
+# T = int(sys.stdin.readline())
+
+# results = []
+# for _ in range(T):
+#     a, b = map(int, sys.stdin.readline().strip().split())
+
+#     results.append((a * b) // returnLCM(a, b))
+
+# for i in results:
+#     print(i)
+
+
+#백준 13241 최소공배수
 import sys
 
-def returnLCM(a, b):
-    while (b != 0):
-        r = a % b
-        a = b
-        b = r
+A, B = map(int, sys.stdin.readline().split())
+
+temp = A * B
+while (B != 0):
+        r = A % B
+        A = B
+        B = r
     
-    return a
+result = temp // A
 
-T = int(sys.stdin.readline())
-
-results = []
-for _ in range(T):
-    a, b = map(int, sys.stdin.readline().strip().split())
-
-    results.append((a * b) // returnLCM(a, b))
-
-for i in results:
-    print(i)
+print(result)
