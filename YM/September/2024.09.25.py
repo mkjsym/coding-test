a784ad8de818363cbca0239c278c9a1ecbcac851
mkjsym <mkjsym@naver.com>
2024-09-25 21:49:17 +0900

    2024.09.25

--- YM/September/2024.09.25.py
+++ YM/September/2024.09.25.py
@@ -7,17 +7,30 @@ queue1 = deque(map(int, sys.stdin.readline().split()))
 queue2 = deque([])
 head = 1
 
-while (True):
-    if (head in queue1):
-        queue2.append(queue1.pop)
-    elif (head in queue2):
-        queue1.append(queue2.pop)
+while (len(queue1) > 0):
+    if (queue1[0] == head):
+        queue1.popleft()
+        head += 1
+        continue
+
+    if (len(queue2) > 0):
+        if (queue2[-1] == head):
+            queue2.pop()
+            head += 1
+            continue
+
+    queue2.append(queue1.popleft())
+
+sel = True
+while (len(queue2) > 0):
+    if (queue2[-1] != head):
+        sel = False
+        break
     else:
-        print('Sad')
-        break
+        queue2.pop()
+        head += 1
 
-    if (head == N):
-        print('Nice')
-        break
-
-    head += 1
+if (sel == True):
+    print('Nice')
+else:
+    print('Sad')
