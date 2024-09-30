75a34a1090a5c6c30fe6aec302139fd9a28751e2
so-yeon1 <psy92411@gmail.com>
2024-09-30 18:12:31 +0900

    2024.09.30

--- SY/Sep/Main30.java
+++ SY/Sep/Main30.java
@@ -1,7 +1,7 @@
 // 2024.09.30
 package SY.Sep;
 
-/******** 15649. N과 M (2) *********/
+/******** 15651. N과 M (2) *********/
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
