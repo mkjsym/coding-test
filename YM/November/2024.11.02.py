5b5f5290fcfe5c00f4adceeb876addc9d584c1c5
mkjsym <mkjsym@naver.com>
2024-11-02 14:35:37 +0900

    2024.11.02

--- YM/November/2024.11.02.py
+++ YM/November/2024.11.02.py
@@ -1,5 +1,19 @@
-#백준 2579 계단 오르기
+#백준 2579 계단 오르기##############################################
 import sys
 input = sys.stdin.readline
 
+N = int(input())
 
+stairs = [0] * 301
+for i in range(1, N + 1):
+    stairs[i] = int(input())
+
+dp = [0] * 301
+dp[1] = stairs[1]
+dp[2] = stairs[1] + stairs[2]
+dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+
+for i in range(4, N + 1):
+    dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i])
+
+print(dp[N])
